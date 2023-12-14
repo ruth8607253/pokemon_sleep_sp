eaea49efb5d313c4f68cdbed69567d2eb213dac5
Ruth <131242482+ruth8607253@users.noreply.githhub.com>
2023-12-14 23:16:04 +0800

    kasumi & koikingu

--- itomaru.py
+++ itomaru.py
@@ -1,4 +1,4 @@
-# イトマル
+# イトマル 圓絲蛛
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.common import NoSuchElementException, ElementNotInteractableException
--- test.py
+++ test.py
@@ -1,53 +1,32 @@
-import sqlite3
-import pandas as pd
+# ...（前面的程式碼保持不變）
 
-class SamePokemon():
-    def __init__(self):
-        self.conn = sqlite3.connect("pokemon_database.db")
-        self.cursor = self.conn.cursor()
-        self.cursor.execute('SELECT * FROM pokemon')
-        self.data = self.cursor.fetchall()
+# 取y值
+y = pokemon_data.iloc[
+    list(range(0, 5)) + list(range(15, 20)) + list(range(33, 36)) +
+    list(range(52, 58)) + list(range(75, 79)) + list(range(81, 84)), 3
+]
 
-    def get_data_as_dataframe(self):
-        columns = [
-            "id",
-            "img_num",
-                "name",
-                "sp",
-                "expertise",
-                "level",
-                "help_fruit",
-                "help_fruit_num",
-                "help_ingredient_1",
-                "help_ingredient_num_1",
-                "help_ingredient_2",
-                "help_ingredient_num_2",
-                "help_ingredient_3",
-                "help_ingredient_num_3",
-                "help_time",
-                "help_max",
-                "skill_main",
-                "skill_main_num",
-                "skill_main_level",
-                "skill_second_1",
-                "skill_second_num_1",
-                "skill_second_2",
-                "skill_second_num_2",
-                "skill_second_3",
-                "skill_second_num_3",
-                "skill_second_4",
-                "skill_second_num_4",
-                "skill_second_5",
-                "skill_second_num_5",
-                "power_up",
-                "power_down","備註"
-        ]
+# 取X值
+X = pokemon_data.iloc[
+    list(range(0, 5)) + list(range(15, 20)) + list(range(33, 36)) +
+    list(range(52, 58)) + list(range(75, 79)) + list(range(81, 84)),
+    [5, 7, 9, 11] + list(range(13, 31))
+]
 
-        df = pd.DataFrame(self.data, columns=columns)
-        return df
+# 對 skill_main 進行 Label Encoding
+from sklearn.preprocessing import LabelEncoder
 
-# 創建一個物件
-pokemon_instance = SamePokemon()
+label_encoder = LabelEncoder()
+X['skill_main_encoded'] = label_encoder.fit_transform(X['skill_main'])
 
-# 將資料以 DataFrame 格式讀取
-pokemon_data = pokemon_instance.get_data_as_dataframe()
+# 除去原始的 skill_main 欄位
+X = X.drop('skill_main', axis=1)
+
+# 繼續對其他類別型欄位做 One-Hot Encoding
+categorical_columns = ['skill_second_1', 'skill_second_2', 'skill_second_3', 'skill_second_4', 'skill_second_5', 'power_up', 'power_down']
+
+for col in categorical_columns:
+    X = pd.concat([X, pd.get_dummies(X[col], prefix=col, drop_first=True)], axis=1)
+    X = X.drop(col, axis=1)
+
+# 接下來進行訓練集和測試集的劃分，並訓練模型...
