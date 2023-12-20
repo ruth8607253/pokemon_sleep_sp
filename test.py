108a8ab6e82ed36fde4a0389cb52663150b37c73
Ruth <131242482+ruth8607253@users.noreply.githhub.com>
2023-12-20 14:28:33 +0800

    img

--- test.py
+++ test.py
@@ -1,32 +1,17 @@
-# ...（前面的程式碼保持不變）
-
-# 取y值
-y = pokemon_data.iloc[
-    list(range(0, 5)) + list(range(15, 20)) + list(range(33, 36)) +
-    list(range(52, 58)) + list(range(75, 79)) + list(range(81, 84)), 3
-]
-
-# 取X值
-X = pokemon_data.iloc[
-    list(range(0, 5)) + list(range(15, 20)) + list(range(33, 36)) +
-    list(range(52, 58)) + list(range(75, 79)) + list(range(81, 84)),
-    [5, 7, 9, 11] + list(range(13, 31))
-]
-
-# 對 skill_main 進行 Label Encoding
-from sklearn.preprocessing import LabelEncoder
-
-label_encoder = LabelEncoder()
-X['skill_main_encoded'] = label_encoder.fit_transform(X['skill_main'])
-
-# 除去原始的 skill_main 欄位
-X = X.drop('skill_main', axis=1)
-
-# 繼續對其他類別型欄位做 One-Hot Encoding
-categorical_columns = ['skill_second_1', 'skill_second_2', 'skill_second_3', 'skill_second_4', 'skill_second_5', 'power_up', 'power_down']
-
-for col in categorical_columns:
-    X = pd.concat([X, pd.get_dummies(X[col], prefix=col, drop_first=True)], axis=1)
-    X = X.drop(col, axis=1)
-
-# 接下來進行訓練集和測試集的劃分，並訓練模型...
+<!DOCTYPE html>
+<html>
+<head>
+    <title>輪播廣告</title>
+</head>
+<body>
+<nav>
+    <div id="PIC">
+        <ul>
+            {% for _ in range(10) %}
+                <li><img src="{{ url_for('static', filename='img/display/') | random_image }}" width="800" height="200" alt=""></li>
+            {% endfor %}
+        </ul>
+    </div>
+</nav>
+</body>
+</html>
--- web.py
+++ web.py
@@ -1,8 +1,65 @@
-from flask import Flask,render_template
-
+from flask import Flask, render_template, request
+import os
+import sqlite3
+import urllib.parse
+import urllib
+import random
 
 app = Flask(__name__)
 
-@app.route("/")
-def main():
-    return render_template("pokemon.html")
+@app.context_processor
+def my_context_processor():
+    search_pokemon = None
+    if request.method == "POST":
+        search_condition = request.form["search_condition"]
+        search_pokemon = None
+        if search_condition:
+            conn = sqlite3.connect("../pokemon_database.db")
+            cursor = conn.cursor()
+            cursor.execute("SELECT * FROM pokemon WHERE img_num=? OR name=?", (search_condition, search_condition))
+            data = cursor.fetchone()
+            if data:
+                search_pokemon = data
+            conn.close()
+    return {"search_pokemon": search_pokemon, "urlparse": urllib.parse, "urllib": urllib}
+
+def my_quote_function(string):
+    return string.replace(" ", "%20")
+
+@app.route("/", methods=["GET", "POST"])
+def index(search_pokemon=None):
+    # 如果是 POST 方法，則處理搜尋
+    if request.method == "POST":
+        search_condition = request.form["search_condition"]
+        if search_condition:
+            conn = sqlite3.connect("../pokemon_database.db")
+            cursor = conn.cursor()
+            cursor.execute("SELECT img_num,name,expertise,help_fruit,help_ingredient_1,skill_main FROM pokemon WHERE img_num=? OR name=?", (search_condition, search_condition))
+            data = cursor.fetchone()
+            if data:
+                search_pokemon = data
+            conn.close()
+        else:
+            return redirect("/random")
+    return render_template("pokemon.html", search_pokemon=search_pokemon)
+
+@app.route("/random", methods=["POST"])
+def random():
+    conn = sqlite3.connect("../pokemon_database.db")
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT img_num,name,expertise,help_fruit,help_ingredient_1,skill_main FROM pokemon ORDER BY RANDOM() LIMIT 1")
+    random_pokemon = cursor.fetchone()
+    conn.close()
+
+    return render_template("pokemon.html", search_pokemon=random_pokemon)
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
+@app.template_filter('random_image')
+def random_image():
+    image_dir = os.path.join(app.static_folder, 'img/display')  # Get the path to the image directory
+    images = [os.path.join(image_dir, f) for f in os.listdir(image_dir) if f.endswith(('.png'))]  # Get a list of image files
+    random_image = random.choice(images)  # Choose a random image
+    return random_image  # Return the path to the random image
